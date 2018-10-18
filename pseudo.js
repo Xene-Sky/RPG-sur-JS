cae379044e0f53ee41d2b710c8986a5bd49145f7
Unknown <pierre.faye@edu.itescia.fr>
2018-10-19 00:25:06 +0200

    Nom obligatoire

--- pseudo.js
+++ pseudo.js
@@ -2,8 +2,17 @@ var nom
 
 function start(){
 	nom = prompt("Veuillez entrer votre Prénom : ");
-	document.getElementById("debut").innerHTML = "Bonjour et Bienvenue, " + nom + " au centre d'enrichisment assisté par ordinateur de la coding Factory";
-	document.getElementById("debut").innerHTML += "<br>" + "Votre échantillon a été traité ; nous pouvons maintenant procéder aux tests.";
-	document.getElementById("debut").innerHTML += "<br>" + "Avant de commencer, vous devez savoir que même si le divertissement et l'apprentissage sont au coeur des activités du centre, vous risquez de subir des lésions irréversibles.";
-	document.getElementById("debut").innerHTML += "<br>" + "Por favor bordón de fallar de muchas gracias de fallar gracias";
+	intro()
+
+}
+function intro(){
+		if (nom == ""){
+		start()
+	}
+	else{
+		document.getElementById("debut").innerHTML = "Bonjour et Bienvenue, " + nom + " au centre d'enrichisment assisté par ordinateur de la coding Factory";
+		document.getElementById("debut").innerHTML += "<br>" + "Votre échantillon a été traité ; nous pouvons maintenant procéder aux tests.";
+		document.getElementById("debut").innerHTML += "<br>" + "Avant de commencer, vous devez savoir que même si le divertissement et l'apprentissage sont au coeur des activités du centre, vous risquez de subir des lésions irréversibles.";
+		document.getElementById("debut").innerHTML += "<br>" + "Por favor bordón de fallar de muchas gracias de fallar gracias";
+	}
 }
