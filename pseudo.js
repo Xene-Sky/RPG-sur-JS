bf48bc30f360aeddc3f13ce609b6e58b65dec76e
Unknown <pierre.faye@edu.itescia.fr>
2018-10-18 18:41:51 +0200

    Nom

---
+++ pseudo.js
@@ -0,0 +1,9 @@
+var nom
+
+function start(){
+	nom = prompt("Veuillez entrer votre Prénom : ");
+	document.getElementById("debut").innerHTML = "Bonjour et Bienvenue, " + nom + " au centre d'enrichisment assisté par ordinateur de la coding Factory";
+	document.getElementById("debut").innerHTML += "<br>" + "Votre échantillon a été traité ; nous pouvons maintenant procéder aux tests.";
+	document.getElementById("debut").innerHTML += "<br>" + "Avant de commencer, vous devez savoir que même si le divertissement et l'apprentissage sont au coeur des activités du centre, vous risquez de subir des lésions irréversibles.";
+	document.getElementById("debut").innerHTML += "<br>" + "Por favor bordón de fallar de muchas gracias de fallar gracias";
+}
